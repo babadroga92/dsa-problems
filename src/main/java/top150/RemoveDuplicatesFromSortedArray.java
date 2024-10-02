5f8929df8d3b7c704143f0ea3a2d4c1e7bc044d3
nemanja <ladro_magro@yahoo.com>
2024-10-02 13:36:00 -0400

    Solution to the problem of removing duplicates in array in place

---
+++ src/main/java/top150/RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,26 @@
+package top150;
+
+public class RemoveDuplicatesFromSortedArray {
+    /*
+        Link to a problem: https://leetcode.com/problems/remove-duplicates-from-sorted-array/description/?envType=study-plan-v2&envId=top-interview-150
+    */
+
+    public int removeDuplicates(int[] array) {
+        // If the array has 0 or 1 element, return the length as no duplicates are possible
+        if (array.length <= 1) return array.length;
+
+        int k = 1; // Initialize k to track the position of the next unique element
+
+        // Loop starts from 1 since the first element is always considered unique
+        for (int i = 1; i < array.length; i++) {
+            // If the current element is not equal to the previous element, it's unique
+            if (array[i] != array[i - 1]) {
+                array[k] = array[i]; // Place the unique element in the next position
+                k++; // Increment k to prepare for the next unique element
+            }
+        }
+        // Return the count of unique elements in the array
+        return k;
+    }
+
+}
