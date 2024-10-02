7c4ca9a693bd2580a2bb00734c36cdda542fdc9c
nemanja <ladro_magro@yahoo.com>
2024-10-02 13:17:08 -0400

    Second problem of Top 150 Leetcode problems solved

---
+++ src/main/java/top150/RemoveElement.java
@@ -0,0 +1,20 @@
+package top150;
+
+public class RemoveElement {
+    /*
+        Link to the problem: https://leetcode.com/problems/remove-element/description/?envType=study-plan-v2&envId=top-interview-150
+    */
+
+    public int removeElement(int[] array, int val) {
+        int count = 0; // Counter for elements that are not equal to 'val'
+
+        for (int i = 0; i < array.length; i++) {
+            int temp = array[i]; // Store current element in a temporary variable
+            if (temp != val) { // Check if current element is not equal to 'val'
+                array[count] = temp; // Move the valid element to the 'count' index
+                count++; // Increment count for valid elements
+            }
+        }
+        return count; // Return the count of valid elements
+    }
+}
